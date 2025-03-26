fa0b41a46a2118b49ae1627d9632783e76fa6105
Max Lemes <maxvlemes@gmail.com>
2025-03-25 23:40:38 -0300

    Primeiro commit

--- previdencia.py
+++ previdencia.py
@@ -1,7 +1,7 @@
-import streamlit as st
+import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
-import numpy as np
+import streamlit as st
 from bs4 import BeautifulSoup
 
 
@@ -18,9 +18,10 @@ def calcular_fv(aporte_mensal, taxa_anual, prazo_anos):
         float: O valor futuro da anuidade.
     """
 
-    taxa_mensal = (1 + taxa_anual / 100) ** (1/12) - 1
+    taxa_mensal = (1 + taxa_anual / 100) ** (1 / 12) - 1
     prazo_meses = prazo_anos * 12
     return aporte_mensal * (((1 + taxa_mensal) ** prazo_meses - 1) / taxa_mensal)
+
 
 def calcular_renda_passiva_anual(aporte_mensal, taxa_anual):
     """
@@ -38,17 +39,18 @@ def calcular_renda_passiva_anual(aporte_mensal, taxa_anual):
     renda_passiva = []
     for ano in anos:
         saldo_acumulado = calcular_fv(aporte_mensal, taxa_anual, ano)
-        rendimento_mensal = saldo_acumulado * ((1 + taxa_anual / 100) ** (1/12) - 1)
+        rendimento_mensal = saldo_acumulado * ((1 + taxa_anual / 100) ** (1 / 12) - 1)
         renda_passiva.append(rendimento_mensal * 12)  # Renda passiva anual
 
     df = pd.DataFrame({"Ano": anos, "Renda Passiva Anual": renda_passiva})
     return df
 
+
 def calcular_renda_passiva_mensal(aporte_mensal, taxa_anual, anos):
     renda_mensal = []
     for ano in anos:
         saldo_acumulado = calcular_fv(aporte_mensal, taxa_anual, ano)
-        rendimento_mensal = saldo_acumulado * ((1 + taxa_anual / 100) ** (1/12) - 1)
+        rendimento_mensal = saldo_acumulado * ((1 + taxa_anual / 100) ** (1 / 12) - 1)
         renda_mensal.append(rendimento_mensal)  # Renda passiva mensal
     return pd.DataFrame({"Ano": anos, "Renda Passiva Mensal": renda_mensal})
 
@@ -88,51 +90,67 @@ def calcular_tabela_prev(ganho, aporte, taxa_anual):
     """
 
     # Valor aportado no 1o mes
-    aporte_1 = ganho*13*aporte/100
+    aporte_1 = ganho * 13 * aporte / 100
 
-    dados = [{
-        "Anos": 1,
-        "Valor Aportado": aporte_1,
-        "Saldo Acumulado": aporte_1,
-        "Renda Passiva Anual": 0,
-        "Renda Passiva Mensal": 0
-    }]
+    dados = [
+        {
+            "Anos": 1,
+            "Valor Aportado": aporte_1,
+            "Saldo Acumulado": aporte_1,
+            "Renda Passiva Anual": 0,
+            "Renda Passiva Mensal": 0,
+        }
+    ]
 
-    anos = list(range(2, 31)) # começa do segundo ano
-    aporte_liq = aporte_1*(1-0.275)
+    anos = list(range(2, 31))  # começa do segundo ano
+    aporte_liq = aporte_1 * (1 - 0.275)
     saldo_acumulado = aporte_1
     for ano in anos:
-        valor_aportado = aporte_1 + (ano-1) * aporte_liq  # Calcula o valor total aportado
-        saldo_acumulado = aporte_1 + saldo_acumulado*(1+taxa_anual/100)
+        valor_aportado = (
+            aporte_1 + (ano - 1) * aporte_liq
+        )  # Calcula o valor total aportado
+        saldo_acumulado = aporte_1 + saldo_acumulado * (1 + taxa_anual / 100)
         if ano < 10:
             rendimento_anual = 0
             rendimento_mensal = 0
         else:
-            rendimento_anual = saldo_acumulado*taxa_anual/100
-            rendimento_mensal = rendimento_anual/12
-        dados.append({
-            "Anos": ano,
-            "Valor Aportado": valor_aportado,
-            "Saldo Acumulado": saldo_acumulado,
-            "Renda Passiva Anual": rendimento_anual,
-            "Renda Passiva Mensal": rendimento_mensal
-        })
+            rendimento_anual = saldo_acumulado * taxa_anual / 100
+            rendimento_mensal = rendimento_anual / 12
+        dados.append(
+            {
+                "Anos": ano,
+                "Valor Aportado": valor_aportado,
+                "Saldo Acumulado": saldo_acumulado,
+                "Renda Passiva Anual": rendimento_anual,
+                "Renda Passiva Mensal": rendimento_mensal,
+            }
+        )
 
     df = pd.DataFrame(dados)
 
     if aporte >= 1:
-        for col in ["Valor Aportado", "Saldo Acumulado","Renda Passiva Anual", "Renda Passiva Mensal"]:
+        for col in [
+            "Valor Aportado",
+            "Saldo Acumulado",
+            "Renda Passiva Anual",
+            "Renda Passiva Mensal",
+        ]:
             df[col] = df[col].apply(lambda x: f"R$ {x:,.2f}")
     else:
-        df["Renda Passiva Mensal"] = df["Renda Passiva Mensal"].apply(lambda x: f"{x:.2%}")
+        df["Renda Passiva Mensal"] = df["Renda Passiva Mensal"].apply(
+            lambda x: f"{x:.2%}"
+        )
 
     return df
+
 
 def calcular_tabela_comparativa_renda(taxa_anual):
     anos = [5, 10, 15, 20, 25, 30]
     dados = {"Anos": anos}
     for aporte in [0.08, 0.10, 0.12]:
-        renda_mensal = calcular_renda_passiva_mensal(aporte, taxa_anual, anos)["Renda Passiva Mensal"]
+        renda_mensal = calcular_renda_passiva_mensal(aporte, taxa_anual, anos)[
+            "Renda Passiva Mensal"
+        ]
         dados[f"Aporte {aporte * 100}%"] = renda_mensal.apply(lambda x: f"{x:.2%}")
     df = pd.DataFrame(dados)
 
@@ -150,18 +168,21 @@ def calcular_tabela_comparativa_patrimonio(taxa_anual):
         df[col] = df[col].apply(lambda x: f"{int(x)}")
     return df
 
+
 def calcular_aporte(ganho_mensal, aporte):
     dados = []
 
-    ganho_anual = 13*ganho_mensal
-    aporte_total = ganho_anual*aporte/100
-    aporte_mensal = aporte_total*(1-0.275)
+    ganho_anual = 13 * ganho_mensal
+    aporte_total = ganho_anual * aporte / 100
+    aporte_mensal = aporte_total * (1 - 0.275)
 
-    dados.append({
-        "Ganho Anual":  ganho_anual,
-        "Aporte 1o Ano": aporte_total,
-        "Aporte 2o Ano": aporte_mensal
-    })
+    dados.append(
+        {
+            "Ganho Anual": ganho_anual,
+            "Aporte 1o Ano": aporte_total,
+            "Aporte 2o Ano": aporte_mensal,
+        }
+    )
 
     df = pd.DataFrame(dados)
 
@@ -169,6 +190,7 @@ def calcular_aporte(ganho_mensal, aporte):
         df[col] = df[col].apply(lambda x: f"R$ {x:,.2f}")
 
     return df
+
 
 def adicionar_linha():
     """Adiciona um espaço entre os frames no Streamlit com uma linha dupla laranja."""
@@ -180,13 +202,16 @@ def adicionar_linha():
     )
     st.markdown(" ")
 
+
 def main():
     st.title("Calculadora de Poupança e Renda")
 
     col1, col2, col3 = st.columns(3)  # Divide a tela em duas colunas
 
     with col1:
-        ganho_mensal = st.number_input("Salário (R$) ", min_value=1, value=5000, step=100)
+        ganho_mensal = st.number_input(
+            "Salário (R$) ", min_value=1, value=5000, step=100
+        )
 
     with col2:
         aporte = st.number_input("Aporte (%)", min_value=1, value=12, step=1)
@@ -194,106 +219,130 @@ def main():
     with col3:
         taxa_anual = st.number_input("Taxa de Juros Anual (%)", min_value=1, value=10)
 
-
     if st.button("Calcular"):
-        adicionar_linha() #-------------------------------------------------------------------------
+        adicionar_linha()  # -------------------------------------------------------------------------
 
-        #--- Calcula o Ganho anual, Aporte Anual e Aporte mensal
+        # --- Calcula o Ganho anual, Aporte Anual e Aporte mensal
 
         tabela = calcular_aporte(ganho_mensal, aporte)
         html = tabela.to_html(index=False)
         # Analisa e modifica o HTML
         soup = BeautifulSoup(html, "html.parser")
-        
+
         for cell in soup.find_all("th"):
             cell["style"] = "text-align: center;"
 
         # Exibe o HTML modificado
         st.write(str(soup), unsafe_allow_html=True)
 
-        adicionar_linha() #-------------------------------------------------------------------------
+        adicionar_linha()  # -------------------------------------------------------------------------
 
-        #--- Calcula a tabela com os valores aportados e o saldo acumulado
+        # --- Calcula a tabela com os valores aportados e o saldo acumulado
 
         tabela = calcular_tabela_prev(ganho_mensal, aporte, taxa_anual)
 
-        df_tab = tabela[tabela['Anos'].isin([5,10,15,20,25,30])]
+        df_tab = tabela[tabela["Anos"].isin([5, 10, 15, 20, 25, 30])]
 
         # Remove o índice ao exibir a tabela
         html = df_tab.to_html(index=False)
-        html = f'<div style="font-size: 16px;">{html}</div>'  # Aumenta o tamanho da fonte
-        
+        html = (
+            f'<div style="font-size: 16px;">{html}</div>'  # Aumenta o tamanho da fonte
+        )
+
         # Analisa e modifica o HTML
         soup = BeautifulSoup(html, "html.parser")
-        
+
         for cell in soup.find_all("th"):
             cell["style"] = "text-align: center;"
 
         # Exibe o HTML modificado
         st.write(str(soup), unsafe_allow_html=True)
 
-        adicionar_linha() #-------------------------------------------------------------------------
+        adicionar_linha()  # -------------------------------------------------------------------------
 
-        #---  Gráfico de barras com o valor aportado e o rendimento
+        # ---  Gráfico de barras com o valor aportado e o rendimento
 
         fig = go.Figure(
-                data=[
-                    go.Bar(
-                        name="Valor Aportado",
-                        x=tabela["Anos"],
-                        y=[float(val[2:].replace(",", "")) for val in tabela["Valor Aportado"]],
-                        hovertemplate="Ano: %{x}<br>Valor Aportado: R$ %{y:,.2f}<extra></extra>",  # Tag informativa
-                    ),
-                    go.Bar(
-                        name="Rendimento",
-                        x=tabela["Anos"],
-                        y=[
-                            float(val[2:].replace(",", "")) - float(val2[2:].replace(",", ""))
-                            for val, val2 in zip(tabela["Saldo Acumulado"], tabela["Valor Aportado"])
-                        ],
+            data=[
+                go.Bar(
+                    name="Valor Aportado",
+                    x=tabela["Anos"],
+                    y=[
+                        float(val[2:].replace(",", ""))
+                        for val in tabela["Valor Aportado"]
+                    ],
+                    hovertemplate="Ano: %{x}<br>Valor Aportado: R$ %{y:,.2f}<extra></extra>",  # Tag informativa
+                ),
+                go.Bar(
+                    name="Rendimento",
+                    x=tabela["Anos"],
+                    y=[
+                        float(val[2:].replace(",", ""))
+                        - float(val2[2:].replace(",", ""))
+                        for val, val2 in zip(
+                            tabela["Saldo Acumulado"], tabela["Valor Aportado"]
+                        )
+                    ],
                     hovertemplate="Ano: %{x}<br>Rendimento: R$ %{y:,.2f}<extra></extra>",  # Tag informativa
-                    ),
-                ]
-            )
+                ),
+            ]
+        )
 
         fig.update_layout(
-            barmode='stack', title='Saldo Acumulado', xaxis_title='Prazo (Anos)', yaxis_title='Valor (R$)'
+            barmode="stack",
+            title="Saldo Acumulado",
+            xaxis_title="Prazo (Anos)",
+            yaxis_title="Valor (R$)",
         )
         st.plotly_chart(fig)
 
-        adicionar_linha() #-------------------------------------------------------------------------
+        adicionar_linha()  # -------------------------------------------------------------------------
 
         tabela_comparativa = calcular_tabela_comparativa_renda(taxa_anual)
 
         # Remove o índice ao exibir a tabela
         html = tabela_comparativa.to_html(index=False)
-        html = f'<div style="font-size: 16px;">{html}</div>'  # Aumenta o tamanho da fonte
+        html = (
+            f'<div style="font-size: 16px;">{html}</div>'  # Aumenta o tamanho da fonte
+        )
 
         # Analisa e modifica o HTML
         soup = BeautifulSoup(html, "html.parser")
-        
+
         for cell in soup.find_all("th"):
             cell["style"] = "text-align: center;"
 
         # Exibe o HTML modificado
-        st.write("Aporte Mensal (% do ganho) x Renda Passiva Mensal (% do salário)", str(soup), unsafe_allow_html=True)
-    
-        adicionar_linha() #-------------------------------------------------------------------------
+        st.write(
+            "Aporte Mensal (% do ganho) x Renda Passiva Mensal (% do salário)",
+            str(soup),
+            unsafe_allow_html=True,
+        )
 
-        tabela_comparativa_patrimonio = calcular_tabela_comparativa_patrimonio(taxa_anual)
+        adicionar_linha()  # -------------------------------------------------------------------------
+
+        tabela_comparativa_patrimonio = calcular_tabela_comparativa_patrimonio(
+            taxa_anual
+        )
 
         # Remove o índice ao exibir a tabela
         html = tabela_comparativa_patrimonio.to_html(index=False)
-        html = f'<div style="font-size: 16px;">{html}</div>'  # Aumenta o tamanho da fonte
+        html = (
+            f'<div style="font-size: 16px;">{html}</div>'  # Aumenta o tamanho da fonte
+        )
 
         # Analisa e modifica o HTML
         soup = BeautifulSoup(html, "html.parser")
-        
+
         for cell in soup.find_all("th"):
             cell["style"] = "text-align: center;"
 
         # Exibe o HTML modificado
-        st.write("Aporte Mensal (% do salário) x Patrimônio (em salários)", str(soup), unsafe_allow_html=True)
+        st.write(
+            "Aporte Mensal (% do salário) x Patrimônio (em salários)",
+            str(soup),
+            unsafe_allow_html=True,
+        )
 
         # Adiciona estilo CSS para centralizar os dados das tabelas
         st.markdown(
@@ -310,7 +359,8 @@ def main():
             unsafe_allow_html=True,
         )
 
-        adicionar_linha() #-------------------------------------------------------------------------
+        adicionar_linha()  # -------------------------------------------------------------------------
+
 
 if __name__ == "__main__":
     main()
--- tabela_sensibilidade.py
+++ tabela_sensibilidade.py
@@ -1,6 +1,6 @@
-import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+import streamlit as st
 
 
 def calcular_fv(aporte_mensal, taxa_anual, prazo_anos):
@@ -16,9 +16,10 @@ def calcular_fv(aporte_mensal, taxa_anual, prazo_anos):
         float: O valor futuro da anuidade.
     """
 
-    taxa_mensal = (1 + taxa_anual / 100) ** (1/12) - 1
+    taxa_mensal = (1 + taxa_anual / 100) ** (1 / 12) - 1
     prazo_meses = prazo_anos * 12
     return aporte_mensal * (((1 + taxa_mensal) ** prazo_meses - 1) / taxa_mensal)
+
 
 def calcular_renda_passiva_anual(aporte_mensal, taxa_anual):
     """
@@ -36,17 +37,18 @@ def calcular_renda_passiva_anual(aporte_mensal, taxa_anual):
     renda_passiva = []
     for ano in anos:
         saldo_acumulado = calcular_fv(aporte_mensal, taxa_anual, ano)
-        rendimento_mensal = saldo_acumulado * ((1 + taxa_anual / 100) ** (1/12) - 1)
+        rendimento_mensal = saldo_acumulado * ((1 + taxa_anual / 100) ** (1 / 12) - 1)
         renda_passiva.append(rendimento_mensal * 12)  # Renda passiva anual
 
     df = pd.DataFrame({"Ano": anos, "Renda Passiva Anual": renda_passiva})
     return df
 
+
 def calcular_renda_passiva_mensal(aporte_mensal, taxa_anual, anos):
     renda_mensal = []
     for ano in anos:
         saldo_acumulado = calcular_fv(aporte_mensal, taxa_anual, ano)
-        rendimento_mensal = saldo_acumulado * ((1 + taxa_anual / 100) ** (1/12) - 1)
+        rendimento_mensal = saldo_acumulado * ((1 + taxa_anual / 100) ** (1 / 12) - 1)
         renda_mensal.append(rendimento_mensal)  # Renda passiva mensal
     return pd.DataFrame({"Ano": anos, "Renda Passiva Mensal": renda_mensal})
 
@@ -91,13 +93,15 @@ def calcular_tabela_5_em_5_anos(aporte_mensal, taxa_anual):
     for ano in anos:
         valor_aportado = aporte_mensal * ano * 12  # Calcula o valor total aportado
         saldo_acumulado = calcular_fv(aporte_mensal, taxa_anual, ano)
-        rendimento_mensal = saldo_acumulado * ((1 + taxa_anual / 100) ** (1/12) - 1)
-        dados.append({
-            "Prazo (Anos)": ano,
-            "Valor Aportado": valor_aportado,
-            "Saldo Acumulado": saldo_acumulado,
-            "Renda Passiva Mensal": rendimento_mensal
-        })
+        rendimento_mensal = saldo_acumulado * ((1 + taxa_anual / 100) ** (1 / 12) - 1)
+        dados.append(
+            {
+                "Prazo (Anos)": ano,
+                "Valor Aportado": valor_aportado,
+                "Saldo Acumulado": saldo_acumulado,
+                "Renda Passiva Mensal": rendimento_mensal,
+            }
+        )
 
     df = pd.DataFrame(dados)
 
@@ -105,7 +109,9 @@ def calcular_tabela_5_em_5_anos(aporte_mensal, taxa_anual):
         for col in ["Valor Aportado", "Saldo Acumulado", "Renda Passiva Mensal"]:
             df[col] = df[col].apply(lambda x: f"R$ {x:,.2f}")
     else:
-        df["Renda Passiva Mensal"] = df["Renda Passiva Mensal"].apply(lambda x: f"{x:.2%}")
+        df["Renda Passiva Mensal"] = df["Renda Passiva Mensal"].apply(
+            lambda x: f"{x:.2%}"
+        )
 
     return df
 
@@ -121,7 +127,9 @@ def calcular_tabela_comparativa_renda(taxa_anual):
     anos = [5, 10, 15, 20, 25, 30]
     dados = {"Anos": anos}
     for aporte in [0.1, 0.15, 0.2, 0.25, 0.3]:
-        renda_mensal = calcular_renda_passiva_mensal(aporte, taxa_anual, anos)["Renda Passiva Mensal"]
+        renda_mensal = calcular_renda_passiva_mensal(aporte, taxa_anual, anos)[
+            "Renda Passiva Mensal"
+        ]
         dados[f"Aporte {aporte * 100}%"] = renda_mensal.apply(lambda x: f"{x:.2%}")
     df = pd.DataFrame(dados)
 
@@ -140,7 +148,6 @@ def calcular_tabela_comparativa_patrimonio(taxa_anual):
     return df
 
 
-
 def main():
     st.title("Calculadora de Poupança e Renda")
 
@@ -150,11 +157,12 @@ def main():
     col1, col2 = st.columns(2)  # Divide a tela em duas colunas
 
     with col1:
-        aporte_mensal = st.number_input("Aporte Mensal", min_value=1, value=1000, step=100)
+        aporte_mensal = st.number_input(
+            "Aporte Mensal", min_value=1, value=1000, step=100
+        )
 
     with col2:
         taxa_anual = st.number_input("Taxa de Juros Anual (%)", min_value=1, value=10)
-
 
     if st.button("Calcular"):
         # Adiciona um espaço entre as tabelas
@@ -174,16 +182,18 @@ def main():
 
         renda_mensal_df = calcular_renda_passiva_mensal(aporte_mensal, taxa_anual, anos)
 
-        df_tab = tabela[tabela['Prazo (Anos)'].isin([5,10,15,20,25,30])]
+        df_tab = tabela[tabela["Prazo (Anos)"].isin([5, 10, 15, 20, 25, 30])]
 
         # Remove o índice ao exibir a tabela
         html = df_tab.to_html(index=False)
-        html = f'<div style="font-size: 16px;">{html}</div>'  # Aumenta o tamanho da fonte
+        html = (
+            f'<div style="font-size: 16px;">{html}</div>'  # Aumenta o tamanho da fonte
+        )
         st.write(df_tab.to_html(index=False), unsafe_allow_html=True)
 
-                # Adiciona um espaço entre as tabelas
+        # Adiciona um espaço entre as tabelas
         st.markdown(" ")  # Você pode adicionar mais espaços para aumentar o espaço
-               # Adiciona uma linha dupla usando CSS
+        # Adiciona uma linha dupla usando CSS
         st.markdown(
             """
             <div style="border-top: 2px double orange; margin-top: 20px; margin-bottom: 20px;"></div>
@@ -192,35 +202,44 @@ def main():
         )
         st.markdown("")  # Você pode adicionar mais espaços para aumentar o espaço
 
-    # Gráfico de barras
+        # Gráfico de barras
         fig = go.Figure(
-                data=[
-                    go.Bar(
-                        name="Valor Aportado",
-                        x=tabela["Prazo (Anos)"],
-                        y=[float(val[2:].replace(",", "")) for val in tabela["Valor Aportado"]],
-                        hovertemplate="Ano: %{x}<br>Valor Aportado: R$ %{y:,.2f}<extra></extra>",  # Tag informativa
-                    ),
-                    go.Bar(
-                        name="Rendimento",
-                        x=tabela["Prazo (Anos)"],
-                        y=[
-                            float(val[2:].replace(",", "")) - float(val2[2:].replace(",", ""))
-                            for val, val2 in zip(tabela["Saldo Acumulado"], tabela["Valor Aportado"])
-                        ],
+            data=[
+                go.Bar(
+                    name="Valor Aportado",
+                    x=tabela["Prazo (Anos)"],
+                    y=[
+                        float(val[2:].replace(",", ""))
+                        for val in tabela["Valor Aportado"]
+                    ],
+                    hovertemplate="Ano: %{x}<br>Valor Aportado: R$ %{y:,.2f}<extra></extra>",  # Tag informativa
+                ),
+                go.Bar(
+                    name="Rendimento",
+                    x=tabela["Prazo (Anos)"],
+                    y=[
+                        float(val[2:].replace(",", ""))
+                        - float(val2[2:].replace(",", ""))
+                        for val, val2 in zip(
+                            tabela["Saldo Acumulado"], tabela["Valor Aportado"]
+                        )
+                    ],
                     hovertemplate="Ano: %{x}<br>Rendimento: R$ %{y:,.2f}<extra></extra>",  # Tag informativa
-                    ),
-                ]
-            )
+                ),
+            ]
+        )
 
         fig.update_layout(
-            barmode='stack', title='Saldo Acumulado', xaxis_title='Prazo (Anos)', yaxis_title='Valor (R$)'
+            barmode="stack",
+            title="Saldo Acumulado",
+            xaxis_title="Prazo (Anos)",
+            yaxis_title="Valor (R$)",
         )
         st.plotly_chart(fig)
 
-                # Adiciona um espaço entre as tabelas
+        # Adiciona um espaço entre as tabelas
         st.markdown(" ")  # Você pode adicionar mais espaços para aumentar o espaço
-               # Adiciona uma linha dupla usando CSS
+        # Adiciona uma linha dupla usando CSS
         st.markdown(
             """
             <div style="border-top: 2px double orange; margin-top: 20px; margin-bottom: 20px;"></div>
@@ -229,7 +248,7 @@ def main():
         )
         st.markdown("")  # Você pode adicionar mais espaços para aumentar o espaço
 
-    # Gráfico de renda passiva mensal
+        # Gráfico de renda passiva mensal
         fig_renda_mensal = go.Figure(
             data=go.Scatter(
                 x=renda_mensal_df["Ano"],
@@ -238,16 +257,18 @@ def main():
                 hovertemplate="Ano: %{x}<br>Renda Mensal: R$ %{y:,.2f}<extra></extra>",  # Tag informativa
             )
         )
-        
+
         fig_renda_mensal.update_layout(
-            title='Renda Passiva Mensal', xaxis_title='Ano', yaxis_title='Renda Mensal (R$)'
+            title="Renda Passiva Mensal",
+            xaxis_title="Ano",
+            yaxis_title="Renda Mensal (R$)",
         )
-        
+
         st.plotly_chart(fig_renda_mensal)
 
-                # Adiciona um espaço entre as tabelas
+        # Adiciona um espaço entre as tabelas
         st.markdown(" ")  # Você pode adicionar mais espaços para aumentar o espaço
-               # Adiciona uma linha dupla usando CSS
+        # Adiciona uma linha dupla usando CSS
         st.markdown(
             """
             <div style="border-top: 2px double orange; margin-top: 20px; margin-bottom: 20px;"></div>
@@ -257,12 +278,16 @@ def main():
         st.markdown("")  # Você pode adicionar mais espaços para aumentar o espaço
 
         tabela_comparativa = calcular_tabela_comparativa_renda(taxa_anual)
-    
-        st.write("Aporte Mensal (% do ganho) x Renda Passiva Mensal (% do salário)", tabela_comparativa.to_html(index=False), unsafe_allow_html=True)
+
+        st.write(
+            "Aporte Mensal (% do ganho) x Renda Passiva Mensal (% do salário)",
+            tabela_comparativa.to_html(index=False),
+            unsafe_allow_html=True,
+        )
 
         # Adiciona um espaço entre as tabelas
         st.markdown(" ")  # Você pode adicionar mais espaços para aumentar o espaço
-               # Adiciona uma linha dupla usando CSS
+        # Adiciona uma linha dupla usando CSS
         st.markdown(
             """
             <div style="border-top: 2px double orange; margin-top: 20px; margin-bottom: 20px;"></div>
@@ -271,9 +296,14 @@ def main():
         )
         st.markdown("")  # Você pode adicionar mais espaços para aumentar o espaço
 
-
-        tabela_comparativa_patrimonio = calcular_tabela_comparativa_patrimonio(taxa_anual)
-        st.write("Aporte Mensal (% do salário) x Patrimônio (em salários)", tabela_comparativa_patrimonio.to_html(index=False), unsafe_allow_html=True)
+        tabela_comparativa_patrimonio = calcular_tabela_comparativa_patrimonio(
+            taxa_anual
+        )
+        st.write(
+            "Aporte Mensal (% do salário) x Patrimônio (em salários)",
+            tabela_comparativa_patrimonio.to_html(index=False),
+            unsafe_allow_html=True,
+        )
 
         # Adiciona estilo CSS para centralizar os dados das tabelas
         st.markdown(
@@ -290,9 +320,9 @@ def main():
             unsafe_allow_html=True,
         )
 
-                # Adiciona um espaço entre as tabelas
+        # Adiciona um espaço entre as tabelas
         st.markdown(" ")  # Você pode adicionar mais espaços para aumentar o espaço
-               # Adiciona uma linha dupla usando CSS
+        # Adiciona uma linha dupla usando CSS
         st.markdown(
             """
             <div style="border-top: 2px double orange; margin-top: 20px; margin-bottom: 20px;"></div>
@@ -300,7 +330,6 @@ def main():
             unsafe_allow_html=True,
         )
         st.markdown("")  # Você pode adicionar mais espaços para aumentar o espaço
-
 
 
 if __name__ == "__main__":
